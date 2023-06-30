3818ef0849c72e03fcce7472ee3f3f77f83d97ca
Frirsta <frista-8@hotmail.com>
2023-06-30 09:40:25 +0200

    Delete localhost from CSRF_TRUSTED_ORIGINS

--- api/settings.py
+++ api/settings.py
@@ -51,7 +51,6 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://snapit-48a119de42d0.herokuapp.com",
     "http://localhost:3000",
-    "localhost:3000",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
