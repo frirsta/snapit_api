5d60e14b3cc7d3bcaea513ea242cac2ed0b9a0ff
Frirsta <frista-8@hotmail.com>
2023-06-30 08:58:15 +0200

    Change cors

--- api/settings.py
+++ api/settings.py
@@ -40,12 +40,13 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
-                 'localhost', '127.0.0.1']
+                 'localhost', '127.0.0.1', "localhost:3000",]
 
 
 CORS_ALLOWED_ORIGINS = [
     "https://snapit-48a119de42d0.herokuapp.com",
     "http://localhost:3000",
+    "localhost:3000",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
