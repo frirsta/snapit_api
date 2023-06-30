258710c1c7a99a099481802449e61fbb4d0fa9f5
Frirsta <frista-8@hotmail.com>
2023-06-30 08:12:46 +0200

    Add localhost to allowed hosts

--- api/settings.py
+++ api/settings.py
@@ -37,10 +37,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
-                 'localhost', '127.0.0.1']
+                 'localhost', '127.0.0.1', 'http://localhost:3000']
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
