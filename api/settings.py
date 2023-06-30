d3b0c94c6d4a1840db4cdbcb192d523aa6c02fa8
Frirsta <frista-8@hotmail.com>
2023-06-30 19:16:49 +0200

    Delete cloudinary field

--- api/settings.py
+++ api/settings.py
@@ -103,8 +103,8 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'refresh-token'
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
