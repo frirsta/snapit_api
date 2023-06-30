d029cc3d393eb3fec9b201ef9b23d4df40b54bc8
Frirsta <frista-8@hotmail.com>
2023-06-30 12:48:47 +0200

    Add filters to follower views

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,16 +1,31 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class UserSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    follower_id = serializers.SerializerMethodField()
+    follower_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+    posts_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_follower_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            follower = Follower.objects.filter(
+                owner=user, following=obj.owner
+            ).first()
+            return follower.id if follower else None
+        return None
+
     class Meta:
         model = Profile
         fields = ['owner', 'is_owner', 'id', 'created_date',
-                  'updated_date', 'bio', 'profile_image']
+                  'updated_date', 'bio', 'profile_image', 'follower_id',
+                  'follower_count', 'following_count', 'posts_count']
--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,40 @@
 from profiles.models import Profile
 from .serializers import UserSerializer
-from rest_framework import generics
+from rest_framework import generics, filters
 from api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Count
 
 # Create your views here.
 
 
 class UserList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        follower_count=Count(
+            'owner__following', distinct=True)).order_by('-created_date')
     serializer_class = UserSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'posts_count',
+        'follower_count',
+        'following_count',
+    ]
+    filterset_fields = [
+        'owner__follower__following__profile',
+        'owner__following__follower__profile'
+    ]
+    search_fields = ['owner__username']
 
 
 class UserDetails(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        follower_count=Count(
+            'owner__following', distinct=True)).order_by('-created_date')
     serializer_class = UserSerializer
