52a5b1495cd67e4d97811c2dbce3a6a8283c9602
frirsta <frirstaalikarem@MacBook-Air-som-tillhor-Frirsta.local>
2023-07-14 18:53:05 +0200

    Change max size image to 4mb

--- posts/serializers.py
+++ posts/serializers.py
@@ -14,9 +14,9 @@ class PostSerializer(serializers.ModelSerializer):
         """
         Throw error 
         """
-        if value.size > 1024 * 1024 * 2:
+        if value.size > 1024 * 1024 * 4:
             raise serializers.ValidationError(
-                'Image size larger than 2MB'
+                'Image size larger than 4MB'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
