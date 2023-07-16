2a6affc30ae4f8ea860fe5773af1bfca6270b92e
Frirsta <frista-8@hotmail.com>
2023-07-16 18:36:08 +0200

    Delete page size

--- api/settings.py
+++ api/settings.py
@@ -40,7 +40,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
